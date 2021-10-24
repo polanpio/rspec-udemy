6b018cc15555b93355930f27e1682b59463aef20
Piotr Polanski <polanski.pio@gmail.com>
2021-10-24 13:41:43 +0900

    refactor with helper method

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,17 +8,21 @@ class Card
 end
 
 RSpec.describe Card do
-  #before do
-  before(:example) do
-    @card = Card.new('Ace', 'Spades')
+  def card
+    Card.new('Ace', 'Spades')
   end
+
+  #before do
+  # before(:example) do
+  #   @card = Card.new('Ace', 'Spades')
+  # end
 
   #specify == it
   it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
   end
 end
