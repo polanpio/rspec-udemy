0c3764a4a3a25b4b97b159478998563f45062811
Piotr Polanski <polanski.pio@gmail.com>
2021-11-27 11:02:08 +0900

    all matcher

--- spec/all_matcher_spec.rb
+++ spec/all_matcher_spec.rb
@@ -2,5 +2,7 @@ RSpec.describe 'all matcher' do
   it 'allows for aggregate checks' do
     expect([5,7,9]).to all(be_odd)
     expect([12,2,4]).to all(be_even)
+    expect([0,0,0]).to all(be_zero)
+    expect([5,7,9]).to all(be < 10)
   end
 end
