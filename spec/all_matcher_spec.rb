a41019dba576650aae45a0caa532f639be6933d9
Piotr Polanski <polanski.pio@gmail.com>
2021-11-27 11:03:52 +0900

    all matcher

--- spec/all_matcher_spec.rb
+++ spec/all_matcher_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe 'all matcher' do
     expect([0,0,0]).to all(be_zero)
     expect([5,7,9]).to all(be < 10)
   end
+
+  describe [2, 4, 10] do
+    it { is_expected.to all(be_even)}
+    it { is_expected.to all(be > 1)}
+  end
 end
